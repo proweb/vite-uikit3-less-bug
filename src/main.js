efd2bc80e00eaa7f47d7e88f17c9d67b75f941ca
Sergey Mochalov <bizezine@gmail.com>
2021-09-13 13:51:28 +0300

    init commit

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import "../node_modules/uikit/src/less/uikit.less";
+
+
+import UIkit from "uikit";
+import Icons from "uikit/dist/js/uikit-icons";
+
+// loads the Icon plugin
+UIkit.use(Icons);
+
+ 
